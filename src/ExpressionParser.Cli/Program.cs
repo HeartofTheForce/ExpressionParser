aa3f78ef4d656bc6d33c9183faaead49d18705fc
Heart <>
2020-10-13 12:51:05 +0200

    add cli project

---
+++ src/ExpressionParser.Cli/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ExpressionParser.Logic;
+
+namespace ExpressionParser.Cli
+{
+    class Program
+    {
+        struct Context
+        {
+            public static readonly Context Empty = new Context();
+        }
+
+        static void Main(string[] args)
+        {
+            string infix = string.Join(' ', args);
+            Console.WriteLine(infix);
+
+            var tokens = Lexer.Process(infix);
+            var postfix = Infix.Infix2Postfix(tokens);
+
+            var function = Compiler.Compile<Context, float>(postfix);
+            float value = function(Context.Empty);
+
+            Console.WriteLine(value);
+        }
+    }
+}
